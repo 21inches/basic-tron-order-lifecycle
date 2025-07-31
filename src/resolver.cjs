c7daed2473a9a749a5590bf0b2ce2c0d75f341c4
xana-rahmani <xanarahmani@gmail.com>
2025-08-01 01:29:33 +0330

    push codes

---
+++ src/resolver.cjs
@@ -0,0 +1,69 @@
+const { Signature } = require("ethers");
+const Sdk = require("@1inch/cross-chain-sdk");
+const { ethers } = require("ethers");
+
+// Load the ABI
+const abiData = JSON.parse(readFileSync('./abi/Resolver.json', 'utf8'));
+const contractABI = abiData.abi;
+
+class Resolver {
+  constructor(srcAddress, lopAddress, tronWeb) {
+    this.srcAddress = srcAddress;
+    this.lopAddress = lopAddress;
+    this.tronWeb = tronWeb;
+    this.contract = await this.tronWeb.contract(contractABI, contractAddress);
+  }
+
+  async deploySrc(
+    chainId,
+    order,
+    signature,
+    takerTraits,
+    amount,
+    hashLock = order.escrowExtension.hashLockInfo
+  ) {
+    const { r, yParityAndS: vs } = Signature.from(signature);
+    const { args, trait } = takerTraits.encode();
+    const immutables = order.toSrcImmutables(
+      chainId,
+      new Sdk.Address(this.srcAddress),
+      amount,
+      hashLock
+    ).build();
+    const hash = this.hashOrder(chainId, order);
+    immutables.orderHash = hash;
+
+    const value = order.escrowExtension.srcSafetyDeposit
+
+    const tx = await this.contract.deploySrc(
+      immutables,
+      order.build(),
+      r,
+      vs,
+      amount,
+      trait,
+      args,
+    ).send({
+      value: value,
+    });
+
+    return tx;
+  }
+
+  hashOrder(srcChainId, order) {
+    const typedData = order.getTypedData(srcChainId);
+    const domain = {
+      name: "1inch Limit Order Protocol",
+      version: "4",
+      chainId: srcChainId,
+      verifyingContract: this.lopAddress,
+    };
+    return ethers.TypedDataEncoder.hash(
+      domain,
+      { Order: typedData.types[typedData.primaryType] },
+      typedData.message
+    );
+  }
+}
+
+module.exports = { Resolver };
