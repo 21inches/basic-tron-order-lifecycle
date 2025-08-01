f020a9fa76edd20d68e79aa91e71fa4cf9ffa730
xana-rahmani <xanarahmani@gmail.com>
2025-08-02 03:24:15 +0330

    updated tron configs

--- config/tron.js
+++ config/tron.js
@@ -5,11 +5,11 @@ export const config = {
     // [TRON] Swap Source Network Configuration
     src: {
         LOP: "0x0656e98bf5b9457048b8ac0985cb48b1b6def4ac", // "TAYjAyuKjKvkhkcvgJ7CgrJ8PVziU5vr4R",
-        EscrowFactory: "0x4fb1926857ec058d3c6f659a376dede5bd41583f", // "THEb78FZnopZYvKpJvRcmicZLcewdmxURR",
-        ResolverContractAddress: "0xb4537cc40fc70b0370d255144935a05159142387", // "TSQgePiJhkeKaqjusfGUe2nRZhGm6sdP8w",
+        EscrowFactory: "0x527eb6a0f425c77722da1d92aa515f691606571b", // "THVQCzNgJxTvBRH297tmHXuxVdcahipy3f",
+        ResolverContractAddress: "0x9afd02fe7b017867e7468a0cacb3546c721edd84", // "TQ6iAAL9oV4Xh6DrQwZ8iGa7q1QAcwhpui",
         USDT: "0xeca9bc828a3005b9a3b909f2cc5c2a54794de05f", //"TXYZopYRdj2D9XRtbG411XZZ3kM5VkAeBf",
-        EscrowSrcImplementationAddress: "0x0bc4bf8569dbbcb793788db183c7dbb4d0b2f80d", // "TB3S8U9PN4TkR4ntfcoNoZDjKiWNgZax7L",
-        EscrowDstImplementationAddress: "0xdd69f5e6be02175b3953de663729b1238ff5f03c", // "TW9wGipDxFLXvqHxBn3euBY3fWBqvExA6t",
+        EscrowSrcImplementationAddress: "0x810deb8c21a11f0f10977378d403c995480c2b8c", // "TMjaqzSMeni2H8qSG2JyShS29dY8zgcm3V",
+        EscrowDstImplementationAddress: "0x724132e32346b5199e7821025bcae3a20c5717fb", // "TLPL921VcESVS3YKB1KnPxNmBENxTDB3jY",
         TrueERC20: "0xf8dfdf1ab75de04f485a9871d9298a070b9bebc6", // "TYf8mVp2tC7K9AYbFFfv8gVH82JEkbKKDj",
         ChainId: 3448148188, // NILE=3448148188,  Mainnet=728126428
         UserPrivateKey: process.env.TRON_SRC_USER_PRIVATE_KEY,
